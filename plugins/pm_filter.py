3704f14ce5de719b9d2c5d04e1e90c734db000e8
axmautofilterbot <103938481+axmautofilterbot@users.noreply.github.com>
2022-05-19 11:09:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -384,10 +384,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             [
                 InlineKeyboardButton('⭕️ Support', url='https://t.me/JOSPSupport'),
                 InlineKeyboardButton('Channel ⭕️', url='https://t.me/JosProjects')
-            ],
-            [
-            ]
-
+            ],          
         try:
             if AUTH_CHANNEL and not await is_subscribed(client, query):
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
